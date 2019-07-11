dba67ae19a23f3b80e81a1cb82e81bb898d88e51
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-11 13:18:46 -0600

    github api connected through axios

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get(`https://api.github.com/users/bryanszendel`)
+.then(data => {
+  console.log('DATA ==>', data)
+})
+.catch(error => {
+  console.log('ERROR', error)
+})
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
